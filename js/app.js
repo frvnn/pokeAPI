14a80e3b5d955e29b2bc361557d53727bebd271c
Franco Vanney <francoavanney@gmail.com>
2021-07-07 12:00:41 -0300

    card con datos

--- js/app.js
+++ js/app.js
@@ -19,9 +19,16 @@ const fetchData = async (id) => {
     try {
         const res = await axios (`https://pokeapi.co/api/v2/pokemon/${id}`)
         
+        console.log(res)
+
         const pokemon = {
             img: res.data.sprites.other.dream_world.front_default,
             nombre: res.data.name,
+            hp: res.data.stats[0].base_stat,
+            experiencia: res.data.base_experience,
+            attack: res.data.stats[1].base_stat,
+            especial: res.data.stats[3].base_stat,
+            defensa: res.data.stats[2].base_stat
         }
 
         pintarCard(pokemon)
@@ -41,7 +48,11 @@ const pintarCard = (pokemon) => {
     const fragment = document.createDocumentFragment()
 
     clone.querySelector('.card-body-img').setAttribute('src', pokemon.img)
-    clone.querySelector('.card-body-title').innerHTML = `${pokemon.nombre} <span>20</span>`
+    clone.querySelector('.card-body-title').innerHTML = `${pokemon.nombre} <span>${pokemon.hp} hp</span>`
+    clone.querySelector('.card-body-text').textContent = pokemon.experiencia + ' Exp'
+    clone.querySelectorAll('.card-footer-social h3')[0].textContent = pokemon.attack +'K'
+    clone.querySelectorAll('.card-footer-social h3')[1].textContent = pokemon.especial +'K'
+    clone.querySelectorAll('.card-footer-social h3')[2].textContent = pokemon.defensa +'K'
     fragment.appendChild(clone)
     flex.appendChild(fragment)
 }   
